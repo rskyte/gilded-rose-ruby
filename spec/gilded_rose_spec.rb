51eb609c5d0332663d34803186a45b726e6abe1c
Rhys Kyte <rskyte@gmail.com>
2018-01-31 12:23:01 +0000

    gilded rose method trimmed and now calls item update_item method

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,13 +4,13 @@ describe GildedRose do
   describe "#update_quality" do
 
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [StandardItem.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
 
     it "cannot cause negative values for quality" do
-      items = [Item.new("foo", 0, 0)]
+      items = [StandardItem.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].to_s).to eq "foo, -1, 0"
     end
